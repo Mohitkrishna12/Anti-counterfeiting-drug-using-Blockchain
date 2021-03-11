7db0098ec9ef813a81a5593458d16b295981a2da
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2021-03-11 20:16:54 +0530

    update login and regiser component

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,41 @@
 import React from 'react'
-import "./Login.css"
+import "./Login.css";
+import {Link} from "react-router-dom";
+import {useStateValue} from "./StateProvider";;
 
 function Login() {
+	const [state, dispatch]=useStateValue();
 	return (
 		<div className="login">
 		<div className="login-container">
-			<h1>Login</h1>
-			<form className="reg_form_layout">
-				<h5>License No:</h5>
+		<div className="login_heading">
+			<h1>Login as {state.role}</h1>
+		</div>
+			<form className="login_form_layout">
+			<div className="login_input">
+				<h4>License No</h4>
 				<input type="text"  />
-				<h5>Password</h5>
+			</div>
+			<div className="login_input">
+				<h4>Password</h4>
 				<input type="password"  />
+			</div>
+				
 				<button className="login_btn" >Sign In</button>
 
 			</form>
 		</div>
-			
+		<div className="login-newaccounttext">
+			<h5>or</h5>
 		</div>
+		<Link to="/register">
+		<button className="login_btn">Register</button>
+		</Link>
+		</div>
+	
+		
+			
+		
 	)
 }
 
--- src/Register.js
+++ src/Register.js
@@ -33,29 +33,29 @@ function Register({props}) {
 		</div>
 		<form className="reg_form_layout">
 		<div className="reg_input"> 
-			<h4>{state.role==="Pharmacurtical Owner"?"Shop":"Company"} Name : <span>*</span> </h4>
+			<h4>{state.role==="Pharmacurtical Owner"?"Shop":"Company"} Name  <span>*</span> </h4>
     	<input type="text" name="name" />
 		</div>
 		<div className="reg_input"> 
-			<h4>Country : <span>*</span> </h4>
+			<h4>Country  <span>*</span> </h4>
     	<input type="text" name="name" />
 		</div>
 		<div className="reg_input"> 
-			<h4>State : <span>*</span> </h4>
+			<h4>State  <span>*</span> </h4>
     	<input type="text" name="name" />
 		</div>
 		<div className="reg_input"> 
-			<h4>Address : <span>*</span> </h4>
+			<h4>Address  <span>*</span> </h4>
     	<textarea rows="4" cols="50" name="comment" />
 		</div>
 		<div className="reg_input">
-		<h4> {regValue()} License : <span>*</span> </h4>
+		<h4> {regValue()} License  <span>*</span> </h4>
 		
 			
     	<input type="text" name="name" />
 		</div>
 		<div className="reg_input"> 
-			<h4>Valid Upto : <span>*</span> </h4>
+			<h4>Valid Upto  <span>*</span> </h4>
     	<input type="date" name="name" />
 		</div>
 		<button className="login_btn">Register</button>	
