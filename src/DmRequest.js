4322b475ebe7828871b6f2ed77ba0433a1b8b11b
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2021-03-19 18:59:54 +0530

    generate unique identification code

--- src/DmRequest.js
+++ src/DmRequest.js
@@ -4,6 +4,7 @@ import "./Upload.css";
 
 function DmRequest() {
 	const [fields, setFields] = useState([{ value: null }]);
+	const [rnum,setrnum]=useState("000000000000");
 
   function handleChange(i, event) {
     const values = [...fields];
@@ -16,8 +17,17 @@ function DmRequest() {
     values.push({ value: null });
     setFields(values);
   }
+  const generateRNUM=()=>{
+  	const num=Math.floor((Math.random()*1000000000000)+1);
+  	setrnum(num);
+  }
 
 	return (
+		<div className="uploadDetails">
+			<div className="details_form">
+			<div className="details_heading">
+			<h1>Request Raw Materials</h1>
+		</div>
 		<div className="DmRequest">
 			<div className="uploadForm">
 			<form className="upload_form_layout"> 
@@ -26,9 +36,14 @@ function DmRequest() {
     		<input type="text" name="name" />
             </div>
             <div className="upload_input" >
-            	<h4>Drug Unique No </h4>
-    		<input type="text" name="name" />
+            	<h4>License No </h4>
+    		<input type="text" name="name"  />
             </div>
+            <div className="upload_input" >
+            	<h4>Drug Unique No </h4>
+    		<input type="text" name="name" value={rnum} />
+            </div>
+            <button className="upload_btn1" type="button" onClick={generateRNUM}>Generate</button>	
 			 {fields.map((field, idx) => {
         return (
 
@@ -46,10 +61,11 @@ function DmRequest() {
         );
       })}
 		
-		<button className="upload_btn1">upload</button>	
+		<button className="upload_btn1">Request</button>	
      
 		</form>	
-				
+				</div>
+				</div>
 			</div>
 		</div>
 	)
